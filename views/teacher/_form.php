a73a56f06bd8deb8df976a55b39e206e355dac65
Kirill Marenov <kmarenov@gmail.com>
2015-09-19 23:32:46 +0300

    teacher CRUD

---
+++ views/teacher/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use app\models\Teacher;
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Teacher */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="teacher-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'gender')->dropDownList(Teacher::getGendersList()) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Добавить' : 'Сохранить',
+            ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ views/teacher/index.php
@@ -0,0 +1,57 @@
+<?php
+
+use app\models\Teacher;
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\TeacherSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Учителя';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="teacher-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Добавить учителя', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'name',
+            [
+                'attribute' => 'gender',
+                'content' => function ($data) {
+                    return $data->getGenderName();
+                },
+                'filter' => Teacher::getGendersList()
+            ],
+            'phone',
+//            [
+//                'attribute'=>'students_count',
+//                'label'=>'Количество учеников',
+//                'format'=>'text',
+//                'content'=>function($data){
+//                    return $data->getStudents()->count();
+//                },
+//            ],
+            [
+                'class' => 'yii\grid\ActionColumn',
+                /*
+                'buttons' => [
+                    'delete' => function(){},
+                    'view' => function(){},
+                ]
+                */
+            ],
+        ],
+    ]); ?>
+
+</div>
