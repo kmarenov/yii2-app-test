51c6aff736f349f84d7bb33f698ffb1e88c10921
Kirill Marenov <kmarenov@gmail.com>
2015-09-19 23:33:20 +0300

    student CRUD

---
+++ views/student/index.php
@@ -0,0 +1,36 @@
+<?php
+
+use yii\grid\GridView;
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\StudentSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Students';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="student-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Student', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'id',
+            'name',
+            'email:email',
+            'birthdate',
+            'level',
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
