12b3e48e5dc5c0616193511a63addc4e437da3e7
Kirill Marenov <kmarenov@gmail.com>
2015-09-23 11:02:58 +0300

    removing basic app rudiments

--- config/console.php
+++ config/console.php
@@ -1,14 +1,11 @@
 <?php
 
-Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
-
-$params = require(__DIR__ . '/params.php');
 $db = require(__DIR__ . '/db.php');
 
 return [
-    'id' => 'basic-console',
+    'id' => 'test-console',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log', 'gii'],
+    'bootstrap' => ['gii'],
     'controllerNamespace' => 'app\commands',
     'modules' => [
         'gii' => 'yii\gii\Module',
@@ -17,15 +14,6 @@ return [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'log' => [
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
         'db' => $db,
     ],
-    'params' => $params,
 ];
--- config/web.php
+++ config/web.php
@@ -1,11 +1,7 @@
 <?php
-
-$params = require(__DIR__ . '/params.php');
-
 $config = [
-    'id' => 'basic',
+    'id' => 'test',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -14,32 +10,8 @@ $config = [
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
-        'user' => [
-            'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
         'db' => require(__DIR__ . '/db.php'),
     ],
-    'params' => $params,
     'language' => 'ru-RU',
     'defaultRoute' => 'teacher/index',
 ];
@@ -49,12 +21,13 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        //'allowedIPs' => ['*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        //'allowedIPs' => ['*']
+        //'allowedIPs' => ['*'],
     ];
 }
 
